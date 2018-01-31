e0d190b9fbc07ec0a7286e86721b8139eeaf6878
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-01-31 21:15:57 +1100

    Fix controller test

--- test/phoenix_and_elm_web/controllers/page_controller_test.exs
+++ test/phoenix_and_elm_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PhoenixAndElmWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Hello PhoenixAndElm!"
   end
 end
