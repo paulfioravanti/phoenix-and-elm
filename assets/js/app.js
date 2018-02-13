0cdf2d6e03b71f16e06455a3b0b689925ce6a4c6
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-02-14 09:47:00 +1030

    Remove more layout code

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,5 +19,7 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-const elmDiv = document.getElementById("elm-main");
-Elm.Main.embed(elmDiv);
+const elmDiv = document.querySelector("#elm-main");
+if (elmDiv) {
+  Elm.Main.embed(elmDiv);
+}
--- lib/phoenix_and_elm_web/views/layout_view.ex
+++ lib/phoenix_and_elm_web/views/layout_view.ex
@@ -1,7 +1,3 @@
 defmodule PhoenixAndElmWeb.LayoutView do
   use PhoenixAndElmWeb, :view
-
-  def socket_url do
-    System.get_env("WEBSOCKET_URL") || "ws://localhost:4000/socket/websocket"
-  end
 end
