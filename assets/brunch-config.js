66702ec28329fd9d362949b32cbbe426a3f5e5bb
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-02-14 12:48:24 +1030

    Attempt to add in 'before' config for brunch

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -6,7 +6,7 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: "js/app.js",
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
@@ -16,12 +16,11 @@ exports.config = {
       // }
       //
       // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
+      order: {
+        before: [
+          "vendor/elm.js"
+        ]
+      }
     },
     stylesheets: {
       joinTo: "css/app.css"
