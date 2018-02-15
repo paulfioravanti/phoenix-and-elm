1ea08a1ad07a953b3a1fbf92cde87575987374a4
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-02-15 11:27:52 +1030

    Add in https to config

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :phoenix_and_elm, PhoenixAndElmWeb.Endpoint,
   load_from_system_env: true,
   url: [
     scheme: "https",
-    host: "shielded-everglades-58659.herokuapp.com",
+    host: "https://shielded-everglades-58659.herokuapp.com",
     # host: "phoenix-1-3-and-elm-http.herokuapp.com",
     port: 443
   ],
