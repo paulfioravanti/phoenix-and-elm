a32c4884e1e7ed7c51e3dd195ded8b34a40bb2ea
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-02-15 15:24:50 +1030

    Add scheme to port

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :phoenix_and_elm, PhoenixAndElmWeb.Endpoint,
   load_from_system_env: true,
   url: [
     scheme: "https",
-    host: "phoenix-1-3-and-elm-websockets.herokuapp.com",
+    host: "https://phoenix-1-3-and-elm-websockets.herokuapp.com",
     port: 443
   ],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
