eef596cb293d91799d9bd60aaaf1c88f155d2e0b
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-02-26 18:15:15 +1100

    Refactor Decoders

--- assets/elm/src/Decoders.elm
+++ assets/elm/src/Decoders.elm
@@ -1,8 +1,8 @@
-module Decoders exposing (..)
+module Decoders exposing (contactListDecoder, contactDecoder)
 
-import Json.Decode as JD exposing (..)
+import Json.Decode as JD exposing (field, int, list, string, succeed)
 import Json.Decode.Extra exposing ((|:))
-import Model exposing (..)
+import Model exposing (Contact, ContactList)
 
 
 contactListDecoder : JD.Decoder ContactList
