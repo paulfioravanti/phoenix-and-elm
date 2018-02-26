d8e056000ace1ca853762f3b5c58418366d19540
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-02-26 17:55:08 +1100

    update Main

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -1,31 +1,32 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Messages exposing (Msg(..))
-import Model exposing (..)
+import Messages exposing (Msg(UrlChange))
+import Model exposing (Flags, Model)
 import Navigation
-import Routing exposing (parse)
-import Subscriptions exposing (subscriptions)
-import Update exposing (..)
-import View exposing (view)
+import Routing
+import Subscriptions
+import Update
+import View
+
+
+main : Program Flags Model Msg
+main =
+    Navigation.programWithFlags
+        UrlChange
+        { init = init
+        , view = View.view
+        , update = Update.update
+        , subscriptions = Subscriptions.subscriptions
+        }
 
 
 init : Flags -> Navigation.Location -> ( Model, Cmd Msg )
 init flags location =
     let
         currentRoute =
-            parse location
+            Routing.parse location
 
         model =
-            initialModel flags currentRoute
+            Model.initialModel flags currentRoute
     in
-        urlUpdate model
-
-
-main : Program Flags Model Msg
-main =
-    Navigation.programWithFlags UrlChange
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+        Update.urlUpdate model
