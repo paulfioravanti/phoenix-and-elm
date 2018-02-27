e903c08762f2fa0608046088d7346289395ead45
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-02-27 18:28:44 +1100

    Add some documentation

--- lib/phoenix_and_elm_web/schema/schema.ex
+++ lib/phoenix_and_elm_web/schema/schema.ex
@@ -6,8 +6,10 @@ defmodule PhoenixAndElmWeb.Schema do
   query do
     @desc "The list of contacts in the address book"
     field :contacts, type: :paginated do
+      @desc "The page number for pagination"
       arg(:page, :integer)
-      arg(:page_size, :integer)
+      # arg(:page_size, :integer)
+      @desc "The query for searching contacts"
       arg(:search, :string)
       resolve(&ContactResolver.search_contacts/3)
     end
--- lib/phoenix_and_elm_web/schema/types.ex
+++ lib/phoenix_and_elm_web/schema/types.ex
@@ -3,30 +3,48 @@ defmodule PhoenixAndElmWeb.Schema.Types do
   alias PhoenixAndElm.AddressBook.Contact
   import_types(Absinthe.Type.Custom)
 
+  @desc "A paginated set of entries from a database"
   union :entries do
     @desc "Union object for all paginated things."
     types([:contact])
     resolve_type(fn %Contact{}, _ -> :contact end)
   end
 
+  @desc "A set of paginated items, including pagination metadata"
   object :paginated do
+    @desc "The total number of entries"
     field(:total_entries, :integer)
+    @desc "The total number of paginated pages"
     field(:total_pages, :integer)
+    @desc "The current page number"
     field(:page_number, :integer)
+    @desc "The number of paginated items per page"
     field(:page_size, :integer)
+    @desc "The list of paginated items"
     field(:entries, list_of(:entries))
   end
 
+  @desc "An address book contact"
   object :contact do
+    @desc "Contact identifier"
     field(:id, :id)
+    @desc "Contact first name"
     field(:first_name, :string)
+    @desc "Contact last name"
     field(:last_name, :string)
+    @desc "Contact gender"
     field(:gender, :integer)
+    @desc "Contact birth date"
     field(:birth_date, :date)
+    @desc "Contact current location"
     field(:location, :string)
+    @desc "Contact phone number"
     field(:phone_number, :string)
+    @desc "Contact email"
     field(:email, :string)
+    @desc "Contact headline"
     field(:headline, :string)
+    @desc "Contact picture"
     field(:picture, :string)
   end
 end
