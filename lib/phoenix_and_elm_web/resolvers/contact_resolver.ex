12fb860c1fa458f6b81d10fb6f01ef72e09412b1
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-02-28 09:18:23 +1100

    Model resolver more closely to rest controller

--- lib/phoenix_and_elm_web/resolvers/contact_resolver.ex
+++ lib/phoenix_and_elm_web/resolvers/contact_resolver.ex
@@ -1,17 +1,14 @@
 defmodule PhoenixAndElmWeb.ContactResolver do
   alias PhoenixAndElm.AddressBook
 
-  def search_contacts(_, %{search: query} = params, _) do
+  def search_contacts(_, params, _) do
     contacts =
-      query
+      params
+      |> Map.get(:search, "")
       |> AddressBook.search_contacts()
       |> AddressBook.paginate_contacts_by(params, :first_name)
 
     {:ok, contacts}
-  end
-
-  def search_contacts(_, _, _) do
-    {:ok, AddressBook.list_contacts()}
   end
 
   def get_contact(_, %{id: id}, _) do
