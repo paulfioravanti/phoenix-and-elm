2b75973ac9ac6ef6602a84c272e9cdf9e065a690
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-03-02 16:48:40 +1100

    Remove comments

--- lib/phoenix_and_elm_web/channels/contact_channel.ex
+++ lib/phoenix_and_elm_web/channels/contact_channel.ex
@@ -31,19 +31,6 @@ defmodule PhoenixAndElmWeb.ContactChannel do
       {:reply, {:error, %{error: "Contact not found"}}, socket}
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  # def handle_in("ping", payload, socket) do
-  #   {:reply, {:ok, payload}, socket}
-  # end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (contact:lobby).
-  # def handle_in("shout", payload, socket) do
-  #   broadcast(socket, "shout", payload)
-  #   {:noreply, socket}
-  # end
-
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
